f013d920c3a67de1d0dadb40cafe9724311056c8
Jan Szejko <janek37@gmail.com>
2022-12-21 18:36:57 +0100

    Refactor and clean up day 21

--- day21.py
+++ day21.py
@@ -1,7 +1,7 @@
 import operator
 import sys
 from dataclasses import dataclass
-from typing import Iterable, Callable, Union, TypeVar
+from typing import Iterable, Callable, TypeVar
 
 OPERATORS = {
     '+': operator.add,
@@ -26,7 +26,10 @@ class Op:
     op: Operator
 
 
-def parse_input(lines: Iterable[str]) -> Iterable[tuple[str, Num | Op]]:
+Monkey = Num | Op
+
+
+def parse_input(lines: Iterable[str]) -> Iterable[tuple[str, Monkey]]:
     for line in lines:
         name, job = line.split(': ')
         if '0' <= job[0] <= '9':
@@ -37,7 +40,7 @@ def parse_input(lines: Iterable[str]) -> Iterable[tuple[str, Num | Op]]:
         yield name, monkey
 
 
-def evaluate(name: str, monkeys: dict[str, Num | Op]) -> tuple[int, bool]:
+def evaluate(name: str, monkeys: dict[str, Monkey]) -> tuple[int, bool]:
     match monkeys[name]:
         case Num(n):
             return n, name != 'humn'
@@ -51,34 +54,45 @@ def evaluate(name: str, monkeys: dict[str, Num | Op]) -> tuple[int, bool]:
             return result, clean
 
 
-def eval_humn(value: int, monkey: str, monkeys: dict[str, Op | Num]):
+def eval_humn(value: int, monkey: str, monkeys: dict[str, Monkey]) -> float:
     monkeys['humn'] = Num(value)
     return evaluate(monkey, monkeys)[0]
 
 
-def main():
-    monkeys = dict(parse_input(line.strip() for line in sys.stdin))
-    print(evaluate('root', monkeys)[0])
-    val1, clean1 = evaluate(monkeys['root'].monkey1, monkeys)
-    val2, clean2 = evaluate(monkeys['root'].monkey2, monkeys)
-    monkey = monkeys['root'].monkey2 if clean1 else monkeys['root'].monkey1
-    value = val1 if clean1 else val2
-    min_humn = -value
-    max_humn = value
+def human_binary_search(root: str, monkeys: dict[str, Monkey]) -> int:
+    monkey, value = get_variable_monkey_and_constant_value(root, monkeys)
 
     def human(v: int):
         return eval_humn(v, monkey, monkeys)
 
+    min_humn = -int(value)
+    max_humn = int(value)
     if human(min_humn) > human(max_humn):
         min_humn, max_humn = max_humn, min_humn
+
     while min_humn != max_humn:
         new_humn = (min_humn + max_humn) // 2
         if human(new_humn) <= value:
             min_humn = new_humn
         if human(new_humn) >= value:
             max_humn = new_humn
-        assert human(min_humn) <= value <= human(max_humn)
-    print(min_humn)
+    return min_humn
+
+
+def get_variable_monkey_and_constant_value(root: str, monkeys: dict[str, Monkey]) -> tuple[str, int]:
+    monkey1 = monkeys[root].monkey1
+    monkey2 = monkeys[root].monkey2
+    val1, clean1 = evaluate(monkey1, monkeys)
+    val2, clean2 = evaluate(monkey2, monkeys)
+    monkey = monkey2 if clean1 else monkey1
+    value = val1 if clean1 else val2
+    return monkey, int(value)
+
+
+def main():
+    monkeys = dict(parse_input(line.strip() for line in sys.stdin))
+    print(int(evaluate('root', monkeys)[0]))
+    print(human_binary_search('root', monkeys))
 
 
 if __name__ == '__main__':
