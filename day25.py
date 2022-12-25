4929479f8abab491095aa31fe435f835d9c354c7
Jan Szejko <janek37@gmail.com>
2022-12-25 10:41:12 +0100

    Change day 25 solution to use an addition table

--- day25.py
+++ day25.py
@@ -2,29 +2,31 @@ import sys
 from functools import reduce
 from itertools import zip_longest
 
-SNAFU_DIGITS = {
-    '2': 2,
-    '1': 1,
-    '0': 0,
-    '-': -1,
-    '=': -2,
+ADDITION_TABLE = {
+    '==': '-1', '=-': '-2', '=0': '0=', '=1': '0-', '=2': '00',
+    '-=': '-2', '--': '0=', '-0': '0-', '-1': '00', '-2': '01',
+    '0=': '0=', '0-': '0-', '00': '00', '01': '01', '02': '02',
+    '1=': '0-', '1-': '00', '10': '01', '11': '02', '12': '1=',
+    '2=': '00', '2-': '01', '20': '02', '21': '1=', '22': '1-',
 }
 
-SNAFU_REVERSE = '012=-'
+
+def add_snafu_digits(d1: str, d2: str) -> str:
+    return ADDITION_TABLE[d1 + d2]
 
 
-def add_snafu_digits(d1: str, d2: str, d3: str = '0') -> str:
-    s = sum(SNAFU_DIGITS[d] for d in (d1, d2, d3))
-    last_digit = (s + 2) % 5 - 2
-    first_digit = (s - last_digit) // 5
-    return SNAFU_REVERSE[first_digit] + SNAFU_REVERSE[last_digit]
+def add_snafu_digits_with_carry(d1: str, d2: str, carry: str) -> str:
+    carry1, s = add_snafu_digits(d1, d2)
+    carry2, s = add_snafu_digits(s, carry)
+    _, final_carry = add_snafu_digits(carry1, carry2)
+    return final_carry + s
 
 
 def add_snafu(n1: str, n2: str) -> str:
     carry = '0'
     result = ''
     for d1, d2 in zip_longest(reversed(n1), reversed(n2), fillvalue='0'):
-        carry, r = add_snafu_digits(d1, d2, carry)
+        carry, r = add_snafu_digits_with_carry(d1, d2, carry)
         result = r + result
     return result
 
