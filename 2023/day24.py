9ca19a83936ab889c9c7e4326e0b9dc889c45707
Jan Szejko <janek37@gmail.com>
2023-12-25 14:34:17 +0100

    Change day 24 solution to avoid using sympy

--- 2023/day24.py
+++ 2023/day24.py
@@ -1,9 +1,8 @@
 import itertools
 import sys
 from collections.abc import Iterator
+from fractions import Fraction
 from typing import NamedTuple
-
-import sympy
 
 
 class XYZ(NamedTuple):
@@ -41,21 +40,50 @@ def find_stone_position(hailstones: list[tuple[XYZ, XYZ]]) -> XYZ:
     pos1, v1 = hailstones[0]
     pos2, v2 = hailstones[1]
     pos3, v3 = hailstones[2]
-    posx, posy, posz, vx, vy, vz, t1, t2, t3 = sympy.symbols("posx posy posz vx vy vz t1 t2 t3", real=True)
-    # 9 equations, 9 unknowns
-    equations = [
-        sympy.Eq(posx + vx * t1, pos1.x + v1.x * t1),
-        sympy.Eq(posy + vy * t1, pos1.y + v1.y * t1),
-        sympy.Eq(posz + vz * t1, pos1.z + v1.z * t1),
-        sympy.Eq(posx + vx * t2, pos2.x + v2.x * t2),
-        sympy.Eq(posy + vy * t2, pos2.y + v2.y * t2),
-        sympy.Eq(posz + vz * t2, pos2.z + v2.z * t2),
-        sympy.Eq(posx + vx * t3, pos3.x + v3.x * t3),
-        sympy.Eq(posy + vy * t3, pos3.y + v3.y * t3),
-        sympy.Eq(posz + vz * t3, pos3.z + v3.z * t3),
+    # Reddit suggested using cross products:
+    # pos + v*t == pos1 + v1*t is equivalent to (pos - pos1) x (v - v1) == 0
+    # so pos x (v1 - v2) + (pos1 - pos2) x v + pos2 x v2 - pos1 x v1 == 0
+
+    cp1 = cross_product(pos1, v1)
+    cp2 = cross_product(pos2, v2)
+    cp3 = cross_product(pos3, v3)
+    linear_system = [
+        [0, v1.z - v2.z, v2.y - v1.y, 0, pos2.z - pos1.z, pos1.y - pos2.y, cp2.x - cp1.x],
+        [v2.z - v1.z, 0, v1.x - v2.x, pos1.z - pos2.z, 0, pos2.x - pos1.x, cp2.y - cp1.y],
+        [v1.y - v2.y, v2.x - v1.x, 0, pos2.y - pos1.y, pos1.x - pos2.x, 0, cp2.z - cp1.z],
+        [0, v1.z - v3.z, v3.y - v1.y, 0, pos3.z - pos1.z, pos1.y - pos3.y, cp3.x - cp1.x],
+        [v3.z - v1.z, 0, v1.x - v3.x, pos1.z - pos3.z, 0, pos3.x - pos1.x, cp3.y - cp1.y],
+        [v1.y - v3.y, v3.x - v1.x, 0, pos3.y - pos1.y, pos1.x - pos3.x, 0, cp3.z - cp1.z],
     ]
-    solution = sympy.solve(equations)[0]  # there's only one solution for my input
-    return XYZ(solution[posx], solution[posy], solution[posz])
+    solution = solve_linear_system(linear_system)
+    assert all(sol.denominator == 1 for sol in solution[:3])
+    return XYZ(int(solution[0]), int(solution[1]), int(solution[2]))
+
+
+def solve_linear_system(linear_system: list[list[int]]) -> list[Fraction]:
+    n = len(linear_system)
+    coefficients = []
+    while linear_system:
+        new_coef, linear_system = gaussian_elimination(linear_system)
+        coefficients.append(new_coef)
+    solution = []
+    for var_no in range(n - 1, -1, -1):
+        solution.append(sum(coef * sol for coef, sol in zip(coefficients[var_no], solution[n - var_no - 2::-1] + [1])))
+    return solution[::-1]
+
+
+def gaussian_elimination(linear_system: list[list[Fraction | int]]) -> tuple[list[Fraction], list[list[Fraction]]]:
+    row_to_eliminate = next(row for row in linear_system if row[0] != 0)
+    coefficients = [Fraction(-value, row_to_eliminate[0]) for value in row_to_eliminate[1:]]
+    new_system = []
+    for row in linear_system:
+        if row != row_to_eliminate:
+            new_system.append([value + row[0]*coefficient for value, coefficient in zip(row[1:], coefficients)])
+    return coefficients, new_system
+
+
+def cross_product(a: XYZ, b: XYZ) -> XYZ:
+    return XYZ(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x)
 
 
 def main():
