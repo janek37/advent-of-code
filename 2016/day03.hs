9f23d7801ab7b568a99eb7847b551e43a9dfa2f1
Jan Szejko <janek37@gmail.com>
2024-04-04 22:27:58 +0200

    Add day 3/2016

---
+++ 2016/day03.hs
@@ -0,0 +1,21 @@
+import System.IO
+
+
+main = do
+    s <- getContents
+    let parsed = map (map read . words) (lines s)
+    print (count isTriangle parsed)
+    print (count isTriangle (concatMap columns (triples parsed)))
+
+
+count p = sum . map (fromEnum . p)
+
+isTriangle [a, b, c] = 2 * max a (max b c) < a + b + c
+
+triples l = case l of
+    [] -> []
+    [x] -> []
+    [x1, x2] -> []
+    (x1:x2:x3:xs) -> (x1, x2, x3):triples xs
+
+columns ([a1, a2, a3], [b1, b2, b3], [c1, c2, c3]) = [[a1, b1, c1], [a2, b2, c2], [a3, b3, c3]]
