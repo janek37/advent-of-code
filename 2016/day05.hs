afa80d07f23ebf1f01ca6b09eb604f865896bfdc
Jan Szejko <janek37@gmail.com>
2024-04-05 22:56:01 +0200

    Add day 5/2016

---
+++ 2016/day05.hs
@@ -0,0 +1,36 @@
+import System.IO
+
+import Crypto.Hash.MD5
+import Data.ByteString.Base16 as Base16
+import Data.ByteString.UTF8 as BU
+import Data.Char (isOctDigit, ord)
+import Data.Foldable
+
+
+main = do
+    s <- getLine
+    let suffixes = findSuffixes s 0
+    let interestingHashes = map (BU.toString . md5digest s) suffixes
+    putStrLn (map (!! 5) (Prelude.take 8 interestingHashes))
+    let betterHashes = filter isBetter interestingHashes
+    let digitPlaces = map digitPlace betterHashes
+    putStrLn (map (digitAt digitPlaces) [0 .. 7])
+
+
+fiveZeros = BU.fromString "00000"
+
+findSuffixes prefix n =
+    if BU.take 5 digest == fiveZeros
+        then n : findSuffixes prefix (n+1)
+        else findSuffixes prefix (n+1)
+    where
+        digest = md5digest prefix n
+
+md5digest prefix n = Base16.encode (hash (BU.fromString (prefix ++ show n)))
+
+isBetter digest = isOctDigit (digest !! 5)
+
+digitPlace digest = (digest !! 6, ord (digest !! 5) - ord '0')
+
+digitAt digitPlaces n = digit
+    where Just (digit, place) = find ((== n) . snd) digitPlaces
