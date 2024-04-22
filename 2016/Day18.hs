ff7df8a18ae0681459f5b798e0e8f0b056ae0518
Jan Szejko <janek37@gmail.com>
2024-04-23 01:06:07 +0200

    Add day 18/2016

--- 2016/Day18.hs
+++ 2016/Day18.hs
@@ -2,5 +2,22 @@ module Day18 where
 
 day18 :: IO ()
 day18 = do
-    s <- getContents
-    print s -- TODO
+    s <- getLine
+    let firstRow = map charToTile s
+    print $ countSafe 40 firstRow
+    print $ countSafe 400000 firstRow
+
+
+data Tile = Safe | Trap deriving (Eq, Show)
+
+countSafe :: Int -> [Tile] -> Int
+countSafe n firstRow = sum [1::Int | row <- rows n firstRow, tile <- row, tile == Safe]
+
+rows :: Int -> [Tile] -> [[Tile]]
+rows n firstRow = take n $ iterate nextRow firstRow
+
+nextRow :: [Tile] -> [Tile]
+nextRow row = zipWith (\a b -> if a == b then Safe else Trap) (Safe:row) (tail row ++ [Safe])
+
+charToTile :: Char -> Tile
+charToTile ch = if ch == '.' then Safe else Trap
