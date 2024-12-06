f39aec7ab2ae9cdb2381e45faa73701f21dd764b
Jan Szejko <janek37@gmail.com>
2024-12-06 15:06:27 +0100

    Optimize day 6/2024 part 2

--- 2024/day06.py
+++ 2024/day06.py
@@ -1,6 +1,6 @@
 import sys
 from enum import Enum
-from typing import Iterator
+from typing import Iterator, Sequence
 
 
 class Direction(tuple[int, int], Enum):
@@ -57,27 +57,85 @@ def simulate_guard(lab: list[str], start: tuple[int, int]) -> set[tuple[int, int
     return visited
 
 
-def is_loop(lab: list[str], start: tuple[int, int]) -> bool:
+JumpMap = dict[tuple[Direction, int], list[int | None]]
+
+
+def make_jump_map(lab: list[str]) -> JumpMap:
+    jump_map = {}
+    for y, row in enumerate(lab):
+        for direction in [Direction.LEFT, Direction.RIGHT]:
+            jump_map[(direction, y)] = make_jumps(row, direction[0])
+
+    for x in range(len(lab[0])):
+        col = [row[x] for row in lab]
+        for direction in [Direction.UP, Direction.DOWN]:
+            jump_map[(direction, x)] = make_jumps(col, direction[1])
+
+    return jump_map
+
+
+def update_jump_map(lab: list[str], jump_map: JumpMap, pos: tuple[int, int]) -> JumpMap:
+    new_jump_map = jump_map.copy()
+    x, y = pos
+    for direction in [Direction.LEFT, Direction.RIGHT]:
+        new_jump_map[(direction, y)] = make_jumps(lab[y], direction[0])
+    for direction in [Direction.UP, Direction.DOWN]:
+        new_jump_map[(direction, x)] = make_jumps([row[x] for row in lab], direction[1])
+    return new_jump_map
+
+
+def make_jumps(line: Sequence[str], direction: int) -> list[int | None]:
+    jumps = [None] * len(line)
+    last_dest = None
+    was_wall = False
+    i = len(line) - 1 if direction == 1 else 0
+    while 0 <= i < len(line):
+        if was_wall:
+            last_dest = i
+        jumps[i] = last_dest
+        was_wall = line[i] == '#'
+        i -= direction
+    return jumps
+
+
+def next_jump(jump_map: JumpMap, pos: tuple[int, int], direction: Direction) -> tuple[tuple[int, int] | None, Direction]:
+    x, y = pos
+    if direction in [Direction.UP, Direction.DOWN]:
+        next_y = jump_map[(direction, x)][y]
+        if next_y is None:
+            return None, direction
+        next_pos = (x, next_y)
+    else:
+        next_x = jump_map[(direction, y)][x]
+        if next_x is None:
+            return None, direction
+        next_pos = (next_x, y)
+    return next_pos, direction.turn_right()
+
+
+def is_loop(jump_map: JumpMap, start: tuple[int, int]) -> bool:
     pos = start
     direction = Direction.UP
     visited = set()
-    while is_inside(lab, pos):
+    while pos is not None:
         if (pos, direction) in visited:
             return True
         visited.add((pos, direction))
-        pos, direction = next_step(lab, pos, direction)
+        pos, direction = next_jump(jump_map, pos, direction)
     return False
 
 
 def find_loops(lab: list[str], start: tuple[int, int]) -> Iterator[tuple[int, int]]:
     candidates = simulate_guard(lab, start)
     candidates.remove(start)
+    jump_map = make_jump_map(lab)
     for pos in candidates:
         x, y = pos
         new_lab = list(lab)
         new_lab[y] = lab[y][:x] + '#' + lab[y][x+1:]
         assert new_lab[y][x] == '#'
-        if is_loop(new_lab, start):
+        new_jump_map = update_jump_map(new_lab, jump_map, pos)
+        if is_loop(new_jump_map, start):
             yield pos
 
 
