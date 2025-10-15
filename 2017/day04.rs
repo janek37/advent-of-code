bcc1d4053ba553ec8004add028ec7a096cf55cb5
Jan Szejko <janek37@gmail.com>
2025-10-15 23:12:26 +0200

    Add day 4/2017

---
+++ 2017/day04.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::collections::HashSet;
+
+fn main() {
+    let passphrases = parse_input();
+    let valid_count = passphrases.iter().filter(is_valid).count();
+    println!("{}", valid_count);
+    let valid_count2 = passphrases.iter().filter(is_valid2).count();
+    println!("{}", valid_count2)
+}
+
+fn parse_input() -> Vec<Vec<String>> {
+    let lines = io::stdin().lines();
+    let mut phrases = Vec::new();
+    for line in lines {
+        phrases.push(line.unwrap().split_whitespace().map(|s| s.to_string()).collect());
+    }
+    phrases
+}
+
+fn is_valid(passphrase: &&Vec<String>) -> bool {
+    let mut words = HashSet::new();
+    for word in passphrase.iter() {
+        if !words.insert(word) {
+            return false
+        }
+    }
+    true
+}
+
+fn is_valid2(passphrase: &&Vec<String>) -> bool {
+    let mut sorted_words = HashSet::new();
+    for word in passphrase.iter() {
+        let mut sorted_word: Vec<char> = word.chars().collect();
+        sorted_word.sort();
+        if !sorted_words.insert(sorted_word) {
+            return false
+        }
+    }
+    true
+}
--- 2017/main.rs
+++ 2017/main.rs
@@ -1,5 +1,6 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {}
