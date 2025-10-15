5f9ec543305b537dcee1f08711322d725808718f
Jan Szejko <janek37@gmail.com>
2025-10-15 18:38:53 +0200

    Add day 1/2017

---
+++ 2017/day01.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+    let _ = io::stdin().read_line(&mut line);
+    let digits = line.trim_end();
+    let mut sum = 0;
+    for (digit, next_digit) in digits.chars().zip(digits.chars().cycle().skip(1)) {
+        if digit == next_digit {
+            sum += digit.to_digit(10).unwrap();
+        }
+    }
+    println!("{}", sum);
+    let mut sum2 = 0;
+    for (digit, next_digit) in digits.chars().zip(digits.chars().cycle().skip(digits.len() / 2)) {
+        if digit == next_digit {
+            sum2 += digit.to_digit(10).unwrap();
+        }
+    }
+    println!("{}", sum2);
+}
