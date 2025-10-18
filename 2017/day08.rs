4b651f9fa2f3d6e9e3401b597a15fa495038f434
Jan Szejko <janek37@gmail.com>
2025-10-18 23:14:52 +0200

    refactor

--- 2017/day08.rs
+++ 2017/day08.rs
@@ -1,19 +1,15 @@
-//! ```cargo
-//! [dependencies]
-//! regex = "1"
-//! ```
 use std::io;
 use std::collections::HashMap;
-use regex::Regex;
 
 fn main() {
     let instructions = parse_input();
     let mut context: HashMap<String, i32> = HashMap::new();
     let mut max_intermediate_value = 0;
     for instruction in instructions {
-        let new_value = run_instruction(&mut context, &instruction);
-        if new_value > max_intermediate_value {
-            max_intermediate_value = new_value
+        if let Some(v) = run_instruction(&mut context, &instruction) {
+            if v > max_intermediate_value {
+                max_intermediate_value = v
+            }
         }
     }
     let max_value = context.values().max().unwrap();
@@ -21,9 +17,9 @@ fn main() {
     println!("{}", max_intermediate_value);
 }
 
-fn run_instruction(context: &mut HashMap<String, i32>, i: &Instruction) -> i32 {
-    let register_value = match context.get(&i.condition_register) {Some(v) => *v, None => 0};
-    let comparison_result = match i.condition_operator {
+fn run_instruction(context: &mut HashMap<String, i32>, i: &Instruction) -> Option<i32> {
+    let register_value = *context.get(&i.condition_register).unwrap_or(&0);
+    let ok = match i.condition_operator {
         Comparison::Equal => register_value == i.condition_value,
         Comparison::Inequal => register_value != i.condition_value,
         Comparison::Less => register_value < i.condition_value,
@@ -31,15 +27,12 @@ fn run_instruction(context: &mut HashMap<String, i32>, i: &Instruction) -> i32 {
         Comparison::LessOrEqual => register_value <= i.condition_value,
         Comparison::GreaterOrEqual => register_value >= i.condition_value,
     };
-    if comparison_result {
-        let affected_register_value = match context.get(&i.affected_register) {
-            Some(v) => *v,
-            None => 0
-        };
-        context.insert(i.affected_register.clone(), affected_register_value + i.summand);
-        affected_register_value + i.summand
+    if ok {
+        let ptr = context.entry(i.affected_register.clone()).or_insert(0);
+        *ptr += i.summand;
+        Some(*ptr)
     } else {
-        0
+        None
     }
 }
 
@@ -62,27 +55,32 @@ struct Instruction {
 
 impl Instruction {
     fn from(s: &String) -> Instruction {
-        let re = Regex::new(r"(.*) (inc|dec) (-?\d+) if (.*) (<|>|==|!=|<=|>=) (-?\d+)").unwrap();
-        let (_, [ar, op, s, cr, cop, cv]) = re.captures(s).unwrap().extract();
+        let parts: Vec<_> = s.split_whitespace().collect();
+        //Regex::new(r"(.*) (inc|dec) (-?\d+) if (.*) (<|>|==|!=|<=|>=) (-?\d+)").unwrap();
+        let ar = parts[0];
+        let op = parts[1];
+        let val = parts[2].parse::<i32>().unwrap();
+        // parts[3] == "if"
+        let cr = parts[4];
+        let cop = parts[5];
+        let cv = parts[6].parse::<i32>().unwrap();
+
+        let condition_operator = match cop {
+            "==" => Comparison::Equal,
+            "!=" => Comparison::Inequal,
+            "<" => Comparison::Less,
+            ">" => Comparison::Greater,
+            "<=" => Comparison::LessOrEqual,
+            ">=" => Comparison::GreaterOrEqual,
+            _ => unreachable!(),
+        };
+
         Instruction {
             affected_register: ar.to_string(),
-            summand: s.parse::<i32>().unwrap() * (if op == "inc" {1} else {-1}),
+            summand: if op == "inc" { val } else { -val },
             condition_register: cr.to_string(),
-            condition_operator:
-            if cop == "==" {
-                Comparison::Equal
-            } else if cop == "!=" {
-                Comparison::Inequal
-            } else if cop == "<" {
-                Comparison::Less
-            } else if cop == ">" {
-                Comparison::Greater
-            } else if cop == "<=" {
-                Comparison::LessOrEqual
-            } else { // if cop == ">=" {
-                Comparison::GreaterOrEqual
-            },
-            condition_value: cv.parse::<i32>().unwrap(),
+            condition_operator,
+            condition_value: cv,
         }
     }
 }
