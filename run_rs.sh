f9ba1f1442ee9c3f584e675643b2b25efe205351
Jan Szejko <janek37@gmail.com>
2025-12-02 21:39:59 +0100

    Remove -q from run_rs.sh

--- run_rs.sh
+++ run_rs.sh
@@ -3,7 +3,7 @@
 
 if [[ -r $year/Cargo.toml ]]; then
   cd $year
-  aocd $day $year | cargo run -rq $day
+  aocd $day $year | cargo run -r $day
 else
   aocd $day $year | rust-script $year/day$day.rs
 fi
