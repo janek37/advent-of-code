4c7950dc0cd925c362e192499c6edbe468a06987
Jan Szejko <janek37@gmail.com>
2025-12-08 20:41:36 +0100

    rename

--- 2025/day08.rs
+++ 2025/day08.rs
@@ -8,27 +8,27 @@ pub fn main() {
     let pairs = get_sorted_pairs(&junctions);
     let sizes = get_circuit_sizes(&junctions, &pairs[..1000]);
     println!("{}", sizes[0] * sizes[1] * sizes[2]);
-    let mut union_sets = UnionSets::new(&junctions);
-    println!("{}", make_connections(&pairs, &mut union_sets));
+    let mut disjoint_sets = DisjointSets::new(&junctions);
+    println!("{}", make_connections(&pairs, &mut disjoint_sets));
 }
 
 fn get_circuit_sizes(junctions: &[Point], pairs: &[(Point, Point)]) -> Vec<usize> {
-    let mut union_sets = UnionSets::new(&junctions);
-    make_connections(pairs, &mut union_sets);
+    let mut disjoint_sets = DisjointSets::new(&junctions);
+    make_connections(pairs, &mut disjoint_sets);
     let mut circuit_sizes: Vec<usize> = junctions
         .iter()
-        .filter(|&&j| union_sets.is_root(j))
-        .map(|&j| union_sets.get_count(j)).collect();
+        .filter(|&&j| disjoint_sets.is_root(j))
+        .map(|&j| disjoint_sets.get_count(j)).collect();
     circuit_sizes.sort_by_key(|&n| -(n as isize));
     circuit_sizes
 }
 
-struct UnionSets<T> {
+struct DisjointSets<T> {
     parents: HashMap<T, T>,
     counts: HashMap<T, usize>,
 }
 
-impl UnionSets<Point> {
+impl DisjointSets<Point> {
     fn new(points: &[Point]) -> Self {
         Self {
             parents: HashMap::new(),
@@ -79,13 +79,13 @@ impl UnionSets<Point> {
     }
 }
 
-fn make_connections(pairs: &[(Point, Point)], union_sets: &mut UnionSets<Point>) -> i32 {
+fn make_connections(pairs: &[(Point, Point)], disjoint_sets: &mut DisjointSets<Point>) -> i32 {
     let mut last_connection = pairs[0];
     let mut connection_count = 0;
-    let max_connections = union_sets.counts.len() - 1;
+    let max_connections = disjoint_sets.counts.len() - 1;
     for &(j1, j2) in pairs {
-        if !union_sets.are_connected(j1, j2) {
-            union_sets.union(j1, j2);
+        if !disjoint_sets.are_connected(j1, j2) {
+            disjoint_sets.union(j1, j2);
             last_connection = (j1, j2);
             connection_count += 1;
             if connection_count == max_connections {
